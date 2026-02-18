9e03134a637a3d4f3a17177144ba2f9432238913
JOSHNA S JOY <joyjoshna224@gmail.com>
2026-02-18 12:08:20 +0530

    Add files via upload

---
+++ remove_duplicates.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize){
+    if(numsSize == 0) return 0;
+
+    int k = 1; // index for next unique element
+
+    for(int i = 1; i < numsSize; i++){
+        if(nums[i] != nums[i - 1]){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
